a1bc397d23ac917796cde5df760bd58e908d4a3a
WalkerAlfaro <alfaro.trelles@gmail.com>
2023-04-22 19:24:54 -0500

    finish projects section

--- src/consts.ts
+++ src/consts.ts
@@ -36,11 +36,17 @@ export const PROJECTS = [
   {
     "projectImage": "/images/projects/rocket.svg",
     "projectTitle": "frontend mentor challenges",
-    "projectDescription": "Retos de programación frontend que ponen a prueba estas habilidades de desarrollo.\nEstos retos fueron resueltos con React, Vue3 o JS vanilla."
+    "projectDescription": "Retos de programación frontend que ponen a prueba estas habilidades de desarrollo.\nEstos retos fueron resueltos con React, Vue3 o JS vanilla.",
+    "projectTechnologies": [
+      "html", "css", "javascript", "react", "vue", "sass", "gulp"
+    ]
   },
   {
     "projectImage": "/images/projects/weather.svg",
     "projectTitle": "estación metereológica",
-    "projectDescription": "Se implementó en una placa ESP8266, mide variables como temperatura y presión.\nUtiliza el protocolo MQTT (IoT) en su comunicación con el backend."
+    "projectDescription": "Se implementó en una placa ESP8266, mide variables como temperatura y presión.\nUtiliza el protocolo MQTT (IoT) en su comunicación con el backend.",
+    "projectTechnologies": [
+      "react", "node", "express", "mongodb", "postman"
+    ]
   },
 ]
