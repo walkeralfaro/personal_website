4159a8111367c8011e4526edb4ac3c9eb7ad88aa
WalkerAlfaro <alfaro.trelles@gmail.com>
2023-04-29 00:11:11 -0500

    Add validations and styles to form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { SendEmail, DataForm } from './SendEmail';
+import styles from './Contact.module.scss';
 
 export const Contact = () => {
   
   const{ register, reset, formState, formState: { errors, isSubmitSuccessful }, handleSubmit } = useForm();
 
-  const onSubmit = ( data: DataForm ) => {
-    SendEmail(data)
+  const onSubmit = ( data ) => {
+    console.log(data);
   };
 
   useEffect(() => {
@@ -25,29 +25,49 @@ export const Contact = () => {
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label htmlFor="name">name</label>
-          <input type="text" {...register('name', {
+          <label htmlFor="name" className={errors.name && styles.inputError}>name</label>
+          <input type="text" id="name" className={errors.name && styles.inputError}
+          {...register('name', {
             required: true,
             pattern: /^[A-Za-z]+$/i,
             minLength: 2,
             maxLength: 30,
           })} />
-          {errors.name?.type === 'required' && <p>El nombre no tiene un formato requerido</p>}
-          {errors.name?.type === 'maxLength' && <p>El nombre debe tener menos de 30 letras</p>}
-          {errors.name?.type === 'minLength' && <p>El nombre debe tener al menos 2 letras</p>}
-          {errors.name?.type === 'pattern' && <p>El nombre sólo debe contener letras</p>}
+          {errors.name?.type === 'required' && <p>El campo es requerido</p>}
+          {errors.name?.type === 'maxLength' && <p>Máximo 30 letras</p>}
+          {errors.name?.type === 'minLength' && <p>Mínimo 2 letras</p>}
+          {errors.name?.type === 'pattern' && <p>Sólo debe contener letras</p>}
         </div>
         <div>
           <label htmlFor="email">correo</label>
-          <input type="email" {...register('email')} />
+          <input type="text" id="email" {...register('email', {
+            required: true, 
+            pattern: /^\S+@\S+$/i,
+            maxLength: 40,
+          })} />
+          {errors.email?.type === 'required' && <p>El campo es requerido</p>}
+          {errors.email?.type === 'pattern' && <p>Ingrese un correo válido</p>}
+          {errors.email?.type === 'maxLength' && <p>Máximo 40 caracteres</p>}
         </div>
         <div>
           <label htmlFor="subject">asunto</label>
-          <input type="text" {...register('subject')} />
+          <input type="text" id="subject" {...register('subject', {
+            required: true,
+            maxLength: 40,
+          })} />
+          {errors.subject?.type === 'required' && <p>El campo es requerido</p>}
+          {errors.subject?.type === 'maxLength' && <p>Máximo 40 letras</p>}
         </div>
         <div>
           <label htmlFor="message">mensaje</label>
-          <input type="text" {...register('message')} />
+          <input type="text" id="message" {...register('message', {
+            required: true,
+            pattern: /^[^<>{}[\]\\\/]*$/i,
+            maxLength: 300,
+          })} />
+          {errors.message?.type === 'required' && <p>El campo es requerido</p>}
+          {errors.message?.type === 'pattern' && <p>No carácteres especiales</p>}
+          {errors.message?.type === 'maxLength' && <p>Maximo 50 palabras</p>}
         </div>
         <input type="submit" value="Enviar" />
 
--- src/env.d.ts
+++ src/env.d.ts
@@ -3,11 +3,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly EMAIL_USER: string;
-  readonly EMAIL_PASS: string;
-  readonly EMAIL_DIR: string;
-  readonly SMTP_HOST: string;
-  readonly SMTP_PORT: number;
+
 }
 
 interface ImportMeta {
