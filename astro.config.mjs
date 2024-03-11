03818cd7268ce4518d6ebdd9e3043fc9df509eff
Walker Alfaro Trelles <dev.walkeralfaro@gmail.com>
2024-03-11 11:31:08 -0500

    test sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
-import partytown from '@astrojs/partytown'
+import partytown from '@astrojs/partytown';
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://walkeralfaro.com',
-  integrations: [
-    react(),
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-  ],
+  integrations: [react(), sitemap(), partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  })]
 });
