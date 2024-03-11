1ff12498ca9fc6f4187bad8aa2d24f329553ab63
Walker Alfaro Trelles <dev.walkeralfaro@gmail.com>
2024-03-11 11:24:18 -0500

    fix google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown'
 import react from "@astrojs/react";
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://walkeralfaro.com',
   integrations: [
     react(),
-    mdx(),
-    sitemap(),
     partytown({
       config: {
         forward: ['dataLayer.push'],
